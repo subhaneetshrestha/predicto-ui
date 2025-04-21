d2aca7c57ba69653249df1c8de338271d5f57e4c
Subhaneet Shrestha <subhaneet.shrestha@eclathealth.com>
2025-04-21 23:08:37 +0545

    added config and tokens

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,62 @@
-import { type ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {
+  type ApplicationConfig,
+  inject,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideCloudinaryLoader } from '@angular/common';
+import {
+  createUrlTreeFromSnapshot,
+  PreloadAllModules,
+  provideRouter,
+  Router,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withPreloading,
+  withRouterConfig,
+  withViewTransitions,
+} from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+import { ENVIRONMENT } from '@core/tokens/environment.token';
+import { environment } from '@environments/environment';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    {
+      provide: ENVIRONMENT,
+      useValue: environment,
+    },
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling(),
+      withViewTransitions({
+        onViewTransitionCreated: ({ transition, to }) => {
+          const router = inject(Router);
+          const toTree = createUrlTreeFromSnapshot(to, []);
+          // Skip the transition if the only thing changing is the fragment and queryParams
+          if (
+            router.isActive(toTree, {
+              paths: 'exact',
+              matrixParams: 'exact',
+              fragment: 'ignored',
+              queryParams: 'ignored',
+            })
+          ) {
+            transition.skipTransition();
+          }
+        },
+      }),
+      withComponentInputBinding(),
+      withRouterConfig({ paramsInheritanceStrategy: 'always', onSameUrlNavigation: 'reload' }),
+      withPreloading(PreloadAllModules),
+    ),
+    provideHttpClient(withFetch(), withInterceptors([])),
+    provideAnimationsAsync(),
+    provideClientHydration(withEventReplay()),
+    provideCloudinaryLoader(''),
+  ],
 };
